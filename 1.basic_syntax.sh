e77826b8ac464ffb65cd533f9caca9e3a7686012
ggj0228 <ggj0228@naver.com>
2025-05-19 15:43:39 +0900

    두번째커밋

---
+++ 1.basic_syntax.sh
@@ -0,0 +1,9 @@
+# 현재 파일 및 폴더더 조회
+ls
+
+# github에 올리기 
+# git config --global user.name "깃헙이름"
+# git config --global user.email "깃헙이메일주소"
+# git add .
+# git commit -m "첫번째커밋"
+# git push origin main 
