baf4a40408a7b9287e788294a7069d4180cf19ae
ggj0228 <ggj0228@naver.com>
2025-05-20 12:37:48 +0900

    4번째커밋

---
+++ 2.user_management.sh
@@ -0,0 +1,13 @@
+# 사용자 추가 및 비밀번호 짖정
+sudo useradd newuser1
+sudo passwd newuser1
+
+# 사용자 변경 : 변경하고자 하는 계정의 비밀번호 입력
+# sudo의 경우 현재 사용자의 비밀번호 입력
+su - newuser1
+
+# chmod는 권한부여 : r(4), w(2), x(1) , u(소유자), g(그룹), o(그외)로 구성
+chmod 644 파일명 # rww
+chmod u+x 파일명 # x권한 u한테 부여
+chmod u-x 파일명
+chmod u=r 파일명
